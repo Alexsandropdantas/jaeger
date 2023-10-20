1fd793bb2ecbb15df7df6289b5813afad72437dd
alexsandro.dantas <alexsandro.dantas@sefaz.pe.gov.br>
2023-10-20 17:39:54 -0300

    Projeto teste para instrumentacao

---
+++ src/main/java/com/example/demo/Controller.java
@@ -0,0 +1,71 @@
+package com.example.demo;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
+
+
+import java.util.Random;
+
+
+@RestController
+@RequestMapping("/hello")
+public class Controller {
+
+    private static final Logger logger = LoggerFactory.getLogger(Controller.class);
+    Random random = new Random();
+    //private final Tracer tracer;
+    
+    private RestTemplate restTemplate;
+    
+    @Value("${spring.application.name}")
+    private String applicationName;
+    
+    @Autowired
+    public Controller(RestTemplate restTemplate) {
+    	this.restTemplate = restTemplate;
+    }
+    
+
+    
+    @GetMapping("/path1")
+    public ResponseEntity<String> path1() {
+        logger.info("Incoming request at {} for request /path1 ", applicationName);
+        doNothingButSleepForSomeTime();
+
+        String response = restTemplate.getForObject("http://localhost:8081/hello/path2", String.class);
+        return ResponseEntity.ok("response from /path1 + " + response);
+    }
+
+    @GetMapping("/path2")
+    public ResponseEntity<String>  path2() {
+        logger.info("Incoming request at {} at /path2", applicationName);
+        doNothingButSleepForSomeTime();
+        String response = restTemplate.getForObject("http://localhost:8082/hello/framework", String.class);
+        return ResponseEntity.ok("response from /path2 + " + response);
+    }
+    
+    @GetMapping("/framework")
+    public ResponseEntity<String>  framework() {
+        logger.info("Incoming request at {} at /framework", applicationName);
+        doNothingButSleepForSomeTime();
+        return ResponseEntity.ok("response from /framework ");
+    }
+
+
+    public void doNothingButSleepForSomeTime() {
+        try {
+            int sleepTime = random.nextInt(1, 3);
+            logger.info("sleeping for " + sleepTime + " seconds");
+            Thread.sleep(sleepTime * 1000);
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        }
+    }
+}
